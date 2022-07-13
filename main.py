ca9834400410f9eb3f1e8a5327750300994addc2
nnaucke <107959098+nnaucke@users.noreply.github.com>
2022-07-13 13:30:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Hello, World!")
+
+
+
+print("Hello, World!")
+
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+  
+if 5 > 2:
+print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
